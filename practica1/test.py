4e449dab1e8c6ebb81b67b9529137a85409433d7
juliansand <julian.gar@hotmail.com>
2017-03-23 20:14:53 -0600

    cambio

--- practica1/test.py
+++ practica1/test.py
@@ -36,6 +36,7 @@ class ServicioTest(TestCase):
     def test_pago(self):
         pago = PagoServicio.objects.get(user_cuenta = "prueba")
         cable = Servicio.objects.get(servicio = "cable")
+        self.assertEquals(pago.tipo_servicio, cable)
         self.assertNotEqual(pago.monto, '0.00')
         self.assertIsNotNone(pago.user_cuenta)
 
